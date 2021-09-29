92e30bdecaea1683d109493be700780d2f6fc596
Sárdi András <sardi.andras@gmail.com>
2021-09-29 14:57:35 +0200

    Create bekero.cpp

---
+++ bekero.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    cout << "Adjon meg ket szamot, szokozzel elvalasztva: ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "A " << a << " es " << b << " szamok osszege: " << c << "." << endl;
+
+    return 0;
+}
